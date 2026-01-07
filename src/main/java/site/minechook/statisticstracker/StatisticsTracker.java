8fce937a8632a0d88b42f2497f6da2114019d19c
MineChook <thomas.cobiac@outlook.com>
2026-01-08 07:43:48 +1030

    Initial commit

---
+++ src/main/java/site/minechook/statisticstracker/StatisticsTracker.java
@@ -0,0 +1,10 @@
+package site.minechook.statisticstracker;
+
+import net.fabricmc.api.ModInitializer;
+
+public class StatisticsTracker implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/site/minechook/statisticstracker/client/StatisticsTrackerClient.java
@@ -0,0 +1,10 @@
+package site.minechook.statisticstracker.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class StatisticsTrackerClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
